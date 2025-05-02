cc77c7fc3433b1c940f06f3e410896c5c45d2d9a
Lucas Contreras <lucastec2002@hotmail.com>
2025-05-02 18:36:29 -0300

    funciona localmente

--- API-Node.JS/index.js
+++ API-Node.JS/index.js
@@ -7,15 +7,21 @@ const bcrypt = require('bcryptjs')
 app.use(express.json())
 
 app.get('/usuarios', async (req, res) => {
-  const usuarios = await prisma.usuario.findMany()
-  res.json(usuarios)
+    const usuarios = await prisma.usuarios.findMany()
+    res.json(usuarios)
 })
 
 app.post('/usuarios', async (req, res) => {
-  const nuevo = await prisma.usuario.create({
-    data: { nombre: req.body.nombre, email: req.body.email, hash_contrasenia: await bcrypt.hash(req.body.password, 10) }
-  })
-  res.json(nuevo)
+    const { nombre, email, password } = req.body
+    const passwordHasheado = await bcrypt.hash(password, 10)
+    const nuevo = await prisma.usuarios.create({
+        data: {
+            nombre,
+            email,
+            hash_contrasenia: passwordHasheado
+        }
+    })
+    res.json({ id: nuevo.id, nombre: nuevo.nombre, email: nuevo.email })
 })
 
 app.listen(3000, () => console.log('API escuchando en http://localhost:3000'))
