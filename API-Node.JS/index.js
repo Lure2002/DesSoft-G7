3bb273bab983ee7e126e30aea0ab44cd6f1111cb
Lucas Contreras <lucastec2002@hotmail.com>
2025-05-02 19:10:52 -0300

    login

--- API-Node.JS/index.js
+++ API-Node.JS/index.js
@@ -35,5 +35,20 @@ app.post('/usuarios', async (req, res) => {
     res.json({ id: nuevo.id, nombre: nuevo.nombre, email: nuevo.email })
 })
 
+app.post('/usuarios/login', async (req, res) => {
+    const { email, password } = req.body
+    const usuario = await prisma.usuarios.findUnique({
+        where: { email }
+    })
+    if (!usuario) {
+        return res.status(404).json({ error: 'Usuario no encontrado' })
+    }
+    const passwordValido = await bcrypt.compare(password, usuario.hash_contrasenia)
+    if (!passwordValido) {
+        return res.status(401).json({ error: 'Contraseña incorrecta' })
+    }
+    res.json({ id: usuario.id, nombre: usuario.nombre, email: usuario.email })
+})
+
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log(`API escuchando en http://localhost:${PORT}`))
