abb8081073f552b13df97e974fc9c1815c3b37d5
Lucas Contreras <lucastec2002@hotmail.com>
2025-05-05 15:23:19 -0300

    add bucket

--- API-Node.JS/firebase.js
+++ API-Node.JS/firebase.js
@@ -8,7 +8,7 @@ if (!credentialsBase64) {
 const serviceAccount = JSON.parse(Buffer.from(credentialsBase64, 'base64').toString('utf8'));
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'tu-bucket.appspot.com', // cambiá esto por tu bucket real
+  storageBucket: 'smartneckless-1.firebasestorage.app', // cambiá esto por tu bucket real
 });
 
 const bucket = admin.storage().bucket();
